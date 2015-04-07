9fe59512030c561e88a5b9dae94ae3842025d20d
Dan Corman <dan.corman@gmail.com>
2015-04-06 22:56:10 -0500

    Arrays, Expects, Inheritance, Objects bug fixes

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -3,14 +3,14 @@ describe("About Arrays", function() {
   // We shall contemplate truth by testing reality, via spec expectations.  
   it("should create arrays", function() {
     var emptyArray = [];
-    expect(typeof(emptyArray)).toBe(object); // A mistake?-- http:javascript.crockford.com/remedial.html
+    expect(typeof(emptyArray)).toBe("object"); // A mistake?-- http:javascript.crockford.com/remedial.html
     expect(emptyArray.length).toBe(0);
 
     var multiTypeArray = [0, 1, "two", function() { return 3; }, {value1: 4, value2: 5}, [6, 7]];
     expect(multiTypeArray[0]).toBe(0);
     expect(multiTypeArray[2]).toBe("two");
     expect(multiTypeArray[3]()).toBe(3);
-    expect(multiTypeArray[4].value1).toBe({value1: 4, value2: 5});
+    expect(multiTypeArray[4].value1).toBe(4);
     expect(multiTypeArray[4]["value2"]).toBe(5);
     expect(multiTypeArray[5][0]).toBe(6);
   });
@@ -23,10 +23,10 @@ describe("About Arrays", function() {
     expect(array).toEqual([1]);
     
     array[1] = 2;
-    expect(array).toEqual([1, 2]);
+    expect(array).toEqual([1,2]);
     
     array.push(3);
-    expect(array).toEqual(1,2,3);
+    expect(array).toEqual([1,2,3]);
   });
 
   it("should understand array length", function() {
@@ -46,12 +46,12 @@ describe("About Arrays", function() {
   it("should slice arrays", function() {
     var array = ["peanut", "butter", "and", "jelly"];
     
-    expect(array.slice(0, 1)).toEqual("peanut");
-    expect(array.slice(0, 2)).toEqual("peanut","butter");
+    expect(array.slice(0, 1)).toEqual(["peanut"]);
+    expect(array.slice(0, 2)).toEqual(["peanut","butter"]);
     expect(array.slice(2, 2)).toEqual(null);
-    expect(array.slice(2, 20)).toEqual("and","jelly");
-    expect(array.slice(3, 0)).toEqual(null);
-    expect(array.slice(3, 100)).toEqual("jelly");
+    expect(array.slice(2, 20)).toEqual(["and","jelly"]);
+    expect(array.slice(3, 0)).toEqual([]]);
+    expect(array.slice(3, 100)).toEqual(["jelly"]);
     expect(array.slice(5, 1)).toEqual(null);
   });
 
@@ -77,21 +77,21 @@ describe("About Arrays", function() {
     var array = [1, 2];
     array.push(3);
 
-    expect(array).toEqual(1,2,3);
+    expect(array).toEqual([1,2,3]);
     
     var poppedValue = array.pop();
     expect(poppedValue).toBe(3);
-    expect(array).toEqual(1,2);
+    expect(array).toEqual([1,2]);
   });
 
   it("should know about shifting arrays", function() {
     var array = [1, 2];
 
     array.unshift(3);
-    expect(array).toEqual(3,1,2);
+    expect(array).toEqual([3,1,2]);
     
     var shiftedValue = array.shift();
-    expect(shiftedValue).toEqual(a);
-    expect(array).toEqual(1,2);
+    expect(shiftedValue).toEqual(3);
+    expect(array).toEqual([1,2]);
   });  
 });
