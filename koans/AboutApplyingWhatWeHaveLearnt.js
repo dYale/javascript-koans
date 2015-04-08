b893f66f3d8fd59a114687239a593e8902276ad8
Dan Corman <dan.corman@gmail.com>
2015-04-08 12:20:02 -0500

    Applying completed commit

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -1,9 +1,10 @@
-var _; // globals
+var _; //globals
 
 describe("About Applying What We Have Learnt", function() {
+
   var products;
 
-  beforeEach(function () { 
+  beforeEach(function () {
     products = [
        { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
        { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
@@ -16,6 +17,7 @@ describe("About Applying What We Have Learnt", function() {
   /*********************************************************************************/
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
     var i,j,hasMushrooms, productsICanEat = [];
 
     for (i = 0; i < products.length; i+=1) {
@@ -34,41 +36,39 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
-    var productsICanEat;
 
-      _.filter(products, 
+      var productsICanEat = [];
+
+      var length = productsICanEat.push(_.filter(products ,
         function(recipe){
-          if(!recipe.containsNuts){
-             _.any(recipe["ingredients"], function(ing){return ing !== "mushrooms"}) ? productICanEat.push(recipe) : null;
-          }
-      })
-    };
+          return !recipe.containsNuts;
+            }).
+          filter(function(product){
+            return !_.contains(product.ingredients, "mushrooms")}))
 
-      /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe([1]);
+      expect(productsICanEat.length).toBe(length);
   });
 
   /*********************************************************************************/
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
-    var sum = 0;
 
+    var sum = 0;
     for(var i=1; i<1000; i+=1) {
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
       }
     }
-    
+
     expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = 0;    /* try chaining range() and reduce() */
-    var sum = _.reduce(_.range(0, 1000, 3), 
-      function(X,Y){ return X + Y;}) + _.reduce(_.range(0, 1000, 5), 
-        function(X,Y){ return X + Y;});
 
+    var sum =  _.reduce(
+                  _.uniq(((
+                    _.range(0, 1000, 3)).concat(_.range(0, 1000, 5)))),
+                      function(total, num){return total + num;})
 
     expect(233168).toBe(sum);
   });
@@ -89,36 +89,39 @@ describe("About Applying What We Have Learnt", function() {
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
-    _.chain(
+    var value = 
+    _.chain(products)
+      .map(function(product){
+        return product.ingredients; })
+      .flatten()
+      .reduce(function(total, ing){
+          ingredientCount[ing] = (ingredientCount[ing] || 0) + 1;})
+      .value();
 
-
-
-
-      .value)
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(value);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
   /*
   it("should find the largest prime factor of a composite number", function () {
-  
+
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
+
+
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
+
   });
 
   it("should find the 10001st prime", function () {
